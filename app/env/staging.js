4a68b858f52b9eb601fd63eee57501255f6170f5
Damon Kreft <me@damonkreft.com>
2015-07-19 17:42:15 +0100

    env configs

--- app/env/staging.js
+++ app/env/staging.js
@@ -1,4 +1,4 @@
 // the nginx elastic beanstalk servers alias one of these env JS files depenending
 // on the APPLICATION_ENV passed through to them (which set in the AWS EB console).
 // that's how we sitch config dependent on the environment
-window.env = "latest";
+window.env = "staging";
