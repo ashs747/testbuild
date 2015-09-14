e44876d03bbeb6fd15613bf87d5762a55011779f
gristoi <ian.gristock@cirrus-connect.com>
2015-09-14 12:04:08 +0100

    comitting changes

--- app/src/react/Routes.jsx
+++ app/src/react/Routes.jsx
@@ -3,6 +3,7 @@ import ReactRouter from 'react-router-ie8';
 import {connect} from 'react-redux';
 
 import Main from './components/Main.jsx';
+import PersonalLearningJourney from './views/PersonalLearningJourney.jsx';
 
 var Route = ReactRouter.Route;
 var DefaultRoute = ReactRouter.Route;
@@ -11,6 +12,9 @@ var NotFoundRoute = ReactRouter.NotFoundRoute;
 var Routes = (
   <Route handler={ReactRouter.RouteHandler}>
     <DefaultRoute handler={connect((state) => state.auth)(Main)} />
+    <Route name="home" path="/" handler={connect((state) => state.auth)(Main)}>
+      <Route name="personal-learning-journey" path="/personal-learning-journey" handler={connect((state) => state)(PersonalLearningJourney)} />
+    </Route>
   </Route>
 );
 
