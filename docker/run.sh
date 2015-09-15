508832fa47f1a7ddb1677eec12bc00ce73a396bf
Matt Cavanagh <matt.cavanagh@darkynt.co.uk>
2015-09-15 12:33:33 +0100

    Updating for new env config

--- docker/run.sh
+++ docker/run.sh
@@ -4,7 +4,7 @@ then
   zip -r $2.zip ./ -x "*.git*" -x "venv/*" -x "node_modules/*"
   aws s3 cp $2.zip s3://cirrus-deployments/soj-portal/$2.zip
   rm $2.zip
-  aws elasticbeanstalk create-application-version --application-name "soj-portal-latest" --version-label $2 --source-bundle S3Bucket=cirrus-deployments,S3Key=soj-portal/$2.zip
+  aws elasticbeanstalk create-application-version --application-name "SOJ Portal" --version-label $2 --source-bundle S3Bucket=cirrus-deployments,S3Key=soj-portal/$2.zip
   aws elasticbeanstalk update-environment --environment-name soj-portal-latest --version-label $2
   #aws s3 rm s3://cirrus-deployments/soj-portal --recursive --exclude "fonts.zip"
 fi
