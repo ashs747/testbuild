f1ba3e6f0c14725659e9e96072b9ccceed168a11
Matt Cavanagh <matt.cavanagh@darkynt.co.uk>
2015-09-22 15:22:25 +0100

    Tabs from nowhere (linting)

--- app/src/react/components/__tests__/li-anchor-test.js
+++ app/src/react/components/__tests__/li-anchor-test.js
@@ -4,64 +4,64 @@ import LiAnchor from '../li-anchor.jsx';
 import {expect, sinon, getMockReactComponent} from 'cirrus/testing/utils';
 
 describe('LI-A wrapper', function(){
-	var stubProps;
+  var stubProps;
   var component;
   var mountedComponent;
   var TestUtils = React.addons.TestUtils;
 
   const stubLiData = {
-  	'text': 'AnchorText',
-  	'url': 'http://www.mything.com/#myLink'
+    'text': 'AnchorText',
+    'url': 'http://www.mything.com/#myLink'
   };
 
   var myComponent = React.createElement(LiAnchor, stubLiData);
   var myChildListItem = React.addons.createFragment(myComponent);
-  
+
   const stubLiDataWithChildList = {
-  	'text': 'AnchorText',
-  	'url': 'http://www.mything.com/#anotherLink',
-  	'childList': [myChildListItem, myChildListItem, myChildListItem]
+    'text': 'AnchorText',
+    'url': 'http://www.mything.com/#anotherLink',
+    'childList': [myChildListItem, myChildListItem, myChildListItem]
   };
 
   describe('Without a child list', () => {
-  	beforeEach(() => {
-  		component = TestUtils.renderIntoDocument(myComponent);
-			mountedComponent = React.findDOMNode(component);
-  	});
+    beforeEach(() => {
+      component = TestUtils.renderIntoDocument(myComponent);
+      mountedComponent = React.findDOMNode(component);
+    });
 
-  	afterEach(() => {
-  		component = null;
-  		mountedComponent = null;
-  	});
+    afterEach(() => {
+      component = null;
+      mountedComponent = null;
+    });
 
 
-  	it('Renders a list item and anchor with some text and a URL', () => {
-			expect(mountedComponent.querySelector('a').text).to.equal('AnchorText');
+    it('Renders a list item and anchor with some text and a URL', () => {
+      expect(mountedComponent.querySelector('a').text).to.equal('AnchorText');
 
-			const aHref = mountedComponent.querySelector('a').href;
-			expect(aHref).to.equal('http://www.mything.com/#myLink');
- 		 });
+      const aHref = mountedComponent.querySelector('a').href;
+      expect(aHref).to.equal('http://www.mything.com/#myLink');
+     });
 
-  	// eg: a defensive test
-  	it('Does not render a child UL if there is no provided childList', () => {
-			expect(mountedComponent.querySelectorAll('ul').length).to.equal(0);
-		});
+    // eg: a defensive test
+    it('Does not render a child UL if there is no provided childList', () => {
+      expect(mountedComponent.querySelectorAll('ul').length).to.equal(0);
+    });
   });
 
-	describe('With a child list', () => {
-  	beforeEach(() => {
-  		component = TestUtils.renderIntoDocument(React.createElement(LiAnchor, stubLiDataWithChildList));
-			mountedComponent = React.findDOMNode(component);
-  	});
+  describe('With a child list', () => {
+    beforeEach(() => {
+      component = TestUtils.renderIntoDocument(React.createElement(LiAnchor, stubLiDataWithChildList));
+      mountedComponent = React.findDOMNode(component);
+    });
 
-  	afterEach(() => {
-  		component = null;
-  		mountedComponent = null;
-  	});
+    afterEach(() => {
+      component = null;
+      mountedComponent = null;
+    });
 
-		it('Renders a nested list item if you provide it a component', () => {
-			expect(mountedComponent.querySelector('ul')).to.exist;
-			expect(mountedComponent.querySelectorAll('ul li a').length).to.equal(3);
-		});
-	});
+    it('Renders a nested list item if you provide it a component', () => {
+      expect(mountedComponent.querySelector('ul')).to.exist;
+      expect(mountedComponent.querySelectorAll('ul li a').length).to.equal(3);
+    });
+  });
 });
