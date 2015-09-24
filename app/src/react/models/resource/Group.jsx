136b3057a43cced14d8f9566878f6298ff77d690
Matthew Tsinontas <matthewtsinontas@gmail.com>
2015-09-24 10:41:44 +0100

    Creating the next level component which itterates over an array of resources passed in and adds a title to the top of the group

---
+++ app/src/react/models/resource/Group.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Display from './Display.jsx';
+
+class Group extends React.Component {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    let resources = this.props.resources.map((resource, i) => {
+      return <Display key={`resource-${i}`} title={resource.title} reference={resource.reference} icon={resource.type.icon} />
+    });
+    return (
+      <div className="resource-group">
+        <h4>{this.props.title}</h4>
+        {resources}
+      </div>
+    );
+  }
+}
+
+Group.propTypes = { resources: React.PropTypes.array };
+export default Group;
---
+++ app/src/react/models/resource/__tests__/groupTest.js
@@ -0,0 +1,49 @@
+import React from 'react/addons';
+import Group from '../Group.jsx';
+import {expect} from 'cirrus/testing/utils';
+
+describe('ResourceGroup', () => {
+
+  var testUtils = React.addons.TestUtils;
+  var props = {
+    title: "Title",
+    resources: [{
+      title: "resource 1",
+      reference: "url here",
+      type: {icon: "icon"}
+    }, {
+      title: "resource 2",
+      reference: "url here",
+      type: {icon: "icon"}
+    }]
+  };
+  var element = React.createElement(Group, props);
+  var component;
+  var mountedComponent;
+
+  beforeEach(function() {
+    component = testUtils.renderIntoDocument(element);
+    mountedComponent = React.findDOMNode(component);
+  });
+
+  afterEach(() => {
+    component = null;
+    mountedComponent = null;
+  });
+
+  it('should render a div with className: resource-group', () => {
+    var components = testUtils.scryRenderedDOMComponentsWithClass(component, 'resource-group');
+    expect(components.length).to.equal(1);
+  });
+
+  it('should render an h4 tag with the title passed through props', () => {
+    var iClass = mountedComponent.querySelector('h4').textContent;
+    expect(iClass).to.equal("Title");
+  });
+
+  it('should render 2 resource displays if 2 resources are passed in', () => {
+    var components = testUtils.scryRenderedDOMComponentsWithClass(component, 'resource-display');
+    expect(components.length).to.equal(2);
+  });
+
+});
