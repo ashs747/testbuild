963df8232ca87b28c55b53f7b3fbb773c2ec178d
Matthew Tsinontas <matthewtsinontas@gmail.com>
2015-09-25 14:27:16 +0100

    Renamed file path in store

--- app/src/redux/store.js
+++ app/src/redux/store.js
@@ -1,6 +1,6 @@
 import {combineReducers, createStore, applyMiddleware} from 'redux';
 import {reducer as auth} from './reducers/authReducer';
-import promiseMiddleware from './middleware/promised-actions';
+import promiseMiddleware from './middleware/promisedActions';
 import {reducer as learningJourney} from './reducers/learningJourneyReducer';
 import thunk from 'redux-thunk';
 
