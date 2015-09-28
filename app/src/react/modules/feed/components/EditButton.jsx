9fba7f01989c27bde25515fcdfb892f65445813a
Matt Cavanagh <matt.cavanagh@darkynt.co.uk>
2015-09-28 12:18:16 +0100

    wireframing

---
+++ app/src/react/modules/feed/components/EditButton.jsx
@@ -0,0 +1,18 @@
+import 'React' from 'react';
+
+export default class EditButton extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    let iconClass = this.props.type;
+
+    return (
+    <a href="#" className={iconClass + '-icon'} onClick={this.props.clickAction}> </a>
+    )
+
+  }
+
+
+}
---
+++ app/src/react/modules/feed/components/Readme.md
@@ -0,0 +1,2 @@
++ You POST a MYESSAGE to a FEED
++ You REPLY to create a COMMENT against a MESSAGE
