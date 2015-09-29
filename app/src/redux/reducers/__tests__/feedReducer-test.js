05d4cae24dcfc0d58b3a0fe061fa45fa14aba3fa
Matt Cavanagh <matt.cavanagh@darkynt.co.uk>
2015-09-29 15:23:47 +0100

    Removed 'done' from tests... will catch that time back up later

--- app/src/redux/reducers/__tests__/feedReducer-test.js
+++ app/src/redux/reducers/__tests__/feedReducer-test.js
@@ -1,34 +1,37 @@
 import {feedStubBasic} from './feedState-stubs';
-import feedReducer from '../feedReducer';
+import {FeedReducer} from '../feedReducer';
 import {expect} from 'cirrus/testing/utils';
 
 var exampleState = feedStubBasic;
 
 
 describe("The Feed-Reducer - Message Board State Handler", () => {
-  it("Changes a post to Allow-Editable when handling a FEED_ALLOW_EDIT action", (done) => {
+  it("Changes a post to Allow-Editable when handling a FEED_ALLOW_EDIT action", () => {
     let allowEditAction = {
       type: 'FEED_ALLOW_EDIT',
       payload: {
         feedID: 'programmeFeed1',
-        guid: 0
+        editing: true,
+        guid: '0'
       }
     }
-    let finalState = feedReducer(exampleState, allowEditAction);
-    expect(finalState.programmeFeed1.messages[0].editing).to.equal(true);
+    let finalState = FeedReducer(exampleState, allowEditAction);
+    expect(finalState.Feeds.programmeFeed1.messages[0].editing).to.equal(true);
+
   });
 
-  it("Changes a comment to Allow-Editable when handling a FEED_ALLOW_EDIT action", (done) => {
+  it("Changes a comment to Allow-Editable when handling a FEED_ALLOW_EDIT action", () => {
     let allowEditAction = {
       type: 'FEED_ALLOW_EDIT',
       payload: {
         feedID: 'programmeFeed1',
-        guid: 22
+        editing: true,
+        guid: '22'
       }
     }
 
-    let finalState = feedReducer(exampleState, allowEditAction);
-    expect(finalState.programmeFeed1.messages[0].comments[0].editing).to.equal(true);
+    let finalState = FeedReducer(exampleState, allowEditAction);
+    expect(finalState.Feeds.programmeFeed1.messages[0].comments[0].editing).to.equal(true);
   });
 });
 
--- app/src/redux/reducers/feedReducer.js
+++ app/src/redux/reducers/feedReducer.js
@@ -1,19 +1,23 @@
 import {FEED_CREATE_MESSAGE, FEED_ALLOW_EDIT} from '../actions/feedActions';
 
-export default Reducer = (state = {}, action) => {
-  console.log('state', state);
+export const FeedReducer = (state = {}, action) => {
+
+  function editPayloadMapper(message) {
+    if (message.guid === action.payload.guid) {
+      message.editing = action.payload.editing;
+    }
+    if (message.comments && (message.comments.length > 0)) {
+      message.comments = message.comments.map(editPayloadMapper);
+    }
+    return message;
+  };
+
   switch (action.type) {
     case FEED_ALLOW_EDIT:
-      let Feed = state.Feeds[action.payload.feedID];
-      Feed.messages.forEach(function editPayloadMapper(message) {
-        if (guid === action.payload.guid) {
-          message.editing = action.payload.editing;
-        }
-
-        if (message.comments.length > 0) {
-          message.comments = message.comments.map(editPayloadMapper);
-        }
-      });
+      var Feed = state.Feeds[action.payload.feedID];
+      var nextState = {...state};
+      nextState.Feeds[action.payload.feedID].messages = Feed.messages.map(editPayloadMapper);
+      return nextState;
       break;
 
     case FEED_CREATE_MESSAGE:
