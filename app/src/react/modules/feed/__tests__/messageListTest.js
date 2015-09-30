028b75b1afbe56e325f44b9a28568cdca9d5fa1a
Matthew Tsinontas <matthewtsinontas@gmail.com>
2015-09-30 12:11:39 +0100

    Finishing off message list and tests

--- app/src/react/modules/feed/__tests__/messageListTest.js
+++ app/src/react/modules/feed/__tests__/messageListTest.js
@@ -1,11 +1,14 @@
 import React from 'react/addons';
 import MessageList from '../components/MessageList.jsx';
 import {expect} from 'cirrus/testing/utils';
+import moment from 'moment-timezone';
 
 describe('MessageList', () => {
 
   var testUtils = React.addons.TestUtils;
-  var props = {};
+  var props = {
+    messages: []
+  };
   var element = React.createElement(MessageList, props);
   var component;
   var mountedComponent;
@@ -23,6 +26,64 @@ describe('MessageList', () => {
   it('it should render a div with className: message-list', () => {
     var components = testUtils.scryRenderedDOMComponentsWithClass(component, 'message-list');
     expect(components.length).to.equal(1);
+  });
+
+  it('should map 2 messages if 2 are passed into mappMessages()', () => {
+    var mapped = component.mapMessages([{
+      id: 1,
+      user: {
+        forename: "Test",
+        surname: "User",
+        profilePic: {
+          id: 1,
+          reference: 'profile-pic'
+        }
+      },
+      content: "This is a status",
+      date: moment(),
+      files: [],
+      comments: [],
+      editable: false,
+      userCanEdit: false
+    }, {
+      id: 2,
+      user: {
+        forename: "Test",
+        surname: "User",
+        profilePic: {
+          id: 2,
+          reference: 'profile-pic'
+        }
+      },
+      content: "This is a status",
+      date: moment(),
+      files: [],
+      comments: [],
+      editable: false,
+      userCanEdit: false
+    }]);
+    expect(mapped.length).to.equal(2);
+  });
+
+  it('should map the correct fields to the correct properties', () => {
+    var mapped = component.mapMessages([{
+      id: 1,
+      user: {
+        forename: "Test",
+        surname: "User",
+        profilePic: {
+          id: 1,
+          reference: 'profile-pic'
+        }
+      },
+      content: "This is a status",
+      date: moment(),
+      files: [],
+      comments: [],
+      editable: false,
+      userCanEdit: false
+    }]);
+    expect(mapped[0].props).to.have.all.keys(['name', 'content', 'date', 'profilePic', 'files', 'comments', 'editable', 'userCanEdit']);
   });
 
 });
--- app/src/react/modules/feed/components/MessageList.jsx
+++ app/src/react/modules/feed/components/MessageList.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Message from './Message.jsx';
 
 /**
   MessageList Component, used to display a list of messages
   Iterates over a list of Message components
 
   Props:
-    comments     - req array (list of comments to display in the list)
+    messages - req array (list of messages to display in the list)
 
   Considerations
     n/a
@@ -16,16 +17,37 @@ class MessageList extends React.Component {
 
   constructor() {
     super();
+    this.mapMessages = this.mapMessages.bind(this);
   }
 
   render() {
+    let messages = this.mapMessages(this.props.messages);
     return (
       <div className="message-list">
-        MessageList
+        {messages}
       </div>
     );
   }
 
+  mapMessages(messages) {
+    let mappedMessages = messages.map(message => {
+      let key = message.id;
+      let name = `${message.user.forename} ${message.user.surname}`;
+      let content = message.content;
+      let date = message.date;
+      let profilePic = message.user.profilePic.reference;
+      let files = message.files;
+      let comments = message.comments;
+      let editable = message.editing;
+      let userCanEdit = message.userCanEdit;
+      return <Message key={key} name={name} content={content} date={date}
+        profilePic={profilePic} files={files} comments={comments}
+        editable={editable} userCanEdit={userCanEdit} />;
+    });
+    return mappedMessages;
+  }
+
 }
 
+MessageList.propTypes = { messages: React.PropTypes.array.isRequired };
 export default MessageList;
