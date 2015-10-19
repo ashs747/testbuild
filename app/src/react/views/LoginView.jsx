42894a437705ebf0fbbc42a1b289248c5e7c06f5
Matthew Tsinontas <matthewtsinontas@gmail.com>
2015-10-19 16:28:41 +0100

    Login view, with css and background stuff

--- app/src/react/views/LoginView.jsx
+++ app/src/react/views/LoginView.jsx
@@ -20,14 +20,24 @@ class LoginView extends React.Component {
 
   render() {
     return (
-      <div className="login">
-        <div className="login-box">
-          <p>Logo Here</p>
-          <form onSubmit={this.onLoginSubmit}>
-            <input id="email" type="email" className="form-control" placeholder="Email" value={this.state.email} onChange={this.changeHandler.bind(this, 'email')}/>
-            <input id="password" type="password" className="form-control" placeholder="Password" value={this.state.password} onChange={this.changeHandler.bind(this, 'password')}/>
-            <Button id="submit" className="btn btn-block" type="submit">Log in</Button>
-          </form>
+      <div className="wrapper">
+        <div className="login-wrapper">
+          <div className="login">
+            <div className="logo">
+              <img src="assets/img/programme-logo.png" alt="logo" />
+            </div>
+            <div className="form">
+              <form onSubmit={this.onLoginSubmit}>
+                <input id="email" type="email" className="form-control" placeholder="Email" value={this.state.email} onChange={this.changeHandler.bind(this, 'email')}/>
+                <input id="password" type="password" className="form-control" placeholder="Password" value={this.state.password} onChange={this.changeHandler.bind(this, 'password')}/>
+                <Button id="submit" className="btn btn-block" type="submit">Log in</Button>
+              </form>
+              <div className="links">
+                <a href="javascript:void(0)">Forgotten Password?</a>
+                <a href="javascript:void(0)">Need help?</a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
