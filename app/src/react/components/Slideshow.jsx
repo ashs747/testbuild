211931558c8cc0e5d2078054ca56da3896cc0b0c
Matthew Tsinontas <matthewtsinontas@gmail.com>
2015-10-20 11:51:14 +0100

    Slideshow component and styling.

--- app/src/react/components/Slideshow.jsx
+++ app/src/react/components/Slideshow.jsx
@@ -12,25 +12,26 @@ class Slideshow extends React.Component {
   }
 
   render() {
-    let content = this.props.slides[this.state.displaySlide];
+    let content = this.props.slides[this.state.displaySlide].content;
+    let className = this.props.slides[this.state.displaySlide].className;
     let totalSlides = this.props.slides.length;
-    let next = (this.props.slides.length > this.state.displaySlide + 1) ? <a onClick={this.nextSlide}>Next <i className="fa fa-chevron-right"></i></a> : null;
-    let prev = (this.state.displaySlide > 0) ? <a onClick={this.prevSlide}> <i className="fa fa-chevron-left"></i>Prev</a> : null;
+    let next = (this.props.slides.length > this.state.displaySlide + 1) ? <span onClick={this.nextSlide}>Next <i className="fa fa-chevron-right"></i></span> : null;
+    let prev = (this.state.displaySlide > 0) ? <span onClick={this.prevSlide}><i className="fa fa-chevron-left"></i> Prev</span> : null;
     let dots = this.mapDots(this.props.slides.length);
     return (
       <div className="slideshow">
-        <div className="display">
+        <div className={`display ${className}`}>
           {content}
-        </div>
-        <div className="nav">
-          <div className="left-arrow col-xs-2">
-            {prev}
-          </div>
-          <div className="dot-nav col-xs-8">
-            {dots}
-          </div>
-          <div className="right-arrow col-xs-2">
-            {next}
+          <div className="nav">
+            <div className="left-arrow arrow col-xs-4">
+              {prev}
+            </div>
+            <div className="dot-nav col-xs-4">
+              {dots}
+            </div>
+            <div className="right-arrow arrow col-xs-4">
+              {next}
+            </div>
           </div>
         </div>
       </div>
@@ -52,8 +53,9 @@ class Slideshow extends React.Component {
   mapDots(length) {
     let dots = [];
     for (var i = 0; i < length; i++) {
+      let className = (i === this.state.displaySlide) ? "active" : "";
       dots.push(
-        <li className="dot-nav-item"><div className="dot" onClick={this.changeSlide.bind(this, i)}>{i}</div></li>
+        <li key={i} className="dot-nav-item"><div className={`dot ${className}`} onClick={this.changeSlide.bind(this, i)}></div></li>
       );
     }
     return (
@@ -70,7 +72,19 @@ class Slideshow extends React.Component {
 }
 
 Slideshow.defaultProps = {
-  slides: [<p>Hello</p>, <p>There</p>, <p>World!</p>]
+  slides: [{
+    content: (<div>content</div>),
+    className: "slide-1"
+  }, {
+    content: (<div>second content</div>),
+    className: "slide-2"
+  }, {
+    content: (<div>third content</div>),
+    className: "slide-3"
+  }, {
+    content: (<div>fourth content</div>),
+    className: "slide-4"
+  }]
 };
 Slideshow.propTypes = {
   slides: React.PropTypes.array.isRequired
