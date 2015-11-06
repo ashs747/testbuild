d1e6e97d9917eb9e4e963b3edabd18ef3e588ab2
Matthew Tsinontas <matthewtsinontas@gmail.com>
2015-11-06 17:30:32 +0000

    Toolkit page view

--- app/src/react/views/ToolkitPageView.jsx
+++ app/src/react/views/ToolkitPageView.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Markdown from 'react-remarkable';
 
 class ToolkitPageView extends React.Component {
 
@@ -6,12 +7,35 @@ class ToolkitPageView extends React.Component {
     super();
   }
 
-  render() {
-    return (
-      <div>ToolkitPageView</div>
-    );
+  componentDidMount() {
+    //action to get toolkit content
   }
 
+  render() {
+    return (
+      <div className="toolkit-page">
+        <div className="header clearfix">
+          <div className="header-icon"><i className={`fa fa-wrench`}></i></div>
+          <h1>Linking actions to purpose and outcomes using SMART</h1>
+          <a className="pdf">Download PDF</a>
+        </div>
+        <div className="body clearfix">
+          <div className="left-bar">
+            <h4>Toolkit Content</h4>
+            <Markdown source="**Toolkit content goes here via markdown**" />
+          </div>
+          <div className="right-bar">
+            <div className="hints-and-tips">
+              <h3>Hints and Tips</h3>
+              <div className="list">
+                <Markdown source="**Toolkit content goes here via markdown**" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ToolkitPageView;
