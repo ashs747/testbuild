f77e3ada44eb3c474821376d5e4bf49df5519a98
Matthew Tsinontas <matthewtsinontas@gmail.com>
2015-11-10 15:02:41 +0000

    Final bits and bobs, upload component to sit on the profile page, just need to implement on the on-boarding

---
+++ app/src/react/components/UploadProfile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class UploadProfile extends React.Component {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="upload-profile">
+        <div className="circle-image">
+          <img src="/assets/img/profile-placeholder.jpg" alt="profile" />
+        </div>
+        <button onClick={this.uploadProfile} className="btn">UPLOAD/CHANGE</button>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</p>
+      </div>
+    );
+  }
+
+  uploadProfile() {
+    console.log("Uploading image");
+  }
+
+}
+
+export default UploadProfile;
--- app/src/react/views/ProfileView.jsx
+++ app/src/react/views/ProfileView.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import FullDataCaptureForm from '../components/FullDataCaptureForm.jsx';
+import UploadProfile from '../components/UploadProfile.jsx';
 import {connect} from 'react-redux';
 
 function mapCaptureFormProps(state) {
   return {
-    title: "",
-    forename: "",
-    surname: "",
-    telephone: "",
-    jobTitle: "",
-    businessArea: "",
-    skype: "",
-    timezone: ""
+    title: "Mr",
+    forename: "Matthew",
+    surname: "Tsinontas",
+    telephone: "07805063052",
+    jobTitle: "Web Developer",
+    businessArea: "Digital",
+    skype: "matthew.tsinontas",
+    timezone: "Europe/London"
     //action: updateUserObject
   };
 };
@@ -26,9 +27,36 @@ class ProfileView extends React.Component {
   //Just keeping the pull request small for ya son ;)
   render() {
     return (
-      <div className="clearfix" style={{padding: "20px"}}>
-        <div style={{float: "right", width: "50%", padding: "20px"}}>
-          <MappedDataCaptureForm />
+      <div className="profile">
+        <div className="header">
+          <div className="header-text">
+            <h1>My profile</h1>
+            <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+              labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.</h6>
+          </div>
+        </div>
+        <div className="main">
+          <div className="main-inner clearfix">
+            <div className="col-sm-4">
+              <UploadProfile />
+              <div className="details-panel">
+                <div className="panel-header">
+                  <h3>My information</h3>
+                </div>
+                <div className="panel-inner">
+                  <h5>Leadership Programme Name 2nd line if needed</h5>
+                  <p>Cohort: 1 Team: 2</p>
+                  <p>Organisation: Organisation name</p>
+                  <p><b>name@email.co.uk</b></p>
+                  <p className="small-text">If these details are incorrect please contact <a href="mailto:">email@email.com <i className="fa fa-chevron-right"></i></a></p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-7 col-sm-offset-1">
+              <MappedDataCaptureForm />
+            </div>
+          </div>
         </div>
       </div>
     );
