3b7f0d261a4694c806b8c80c97f68a86434d9e83
Matt Cavanagh <matt.cavanagh@darkynt.co.uk>
2015-11-10 12:55:57 +0000

    try-catch the promise wrapper

--- app/src/redux/middleware/promisedActions.js
+++ app/src/redux/middleware/promisedActions.js
@@ -3,32 +3,36 @@ function isPromise(obj) {
   return (obj && (obj instanceof Promise || typeof(obj.then) === 'function'));
 };
 
-export default function promiseMiddleware() {
+function promiseMiddleware() {
   return next => action => {
     if (action && !action.payload || !isPromise(action.payload)) {
       return next(action);
     }
 
-    next({
-      ...action,
-      status: 'PENDING'
-    });
+    try {
+      next({
+        ...action,
+        status: 'PENDING'
+      });
 
-    return action.payload.then(value => {
-      next({
-        ...action,
-        payload: value,
-        status: 'RESOLVED'
+      return action.payload.then((value) => {
+        next({
+          ...action,
+          payload: value,
+          status: 'RESOLVED'
+        });
+      }, (err) => {
+        next({
+          ...action,
+          payload: err,
+          error: true,
+          status: 'REJECTED'
+        });
       });
-    })
-    .catch((err) => {
-      next({
-        ...action,
-        payload: err,
-        error: true,
-        status: 'REJECTED'
-      });
-    });
+    } catch (e) {
+      alert(e);
+    }
   };
 }
 
+export default promiseMiddleware;
