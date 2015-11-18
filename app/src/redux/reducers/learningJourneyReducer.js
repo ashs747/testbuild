958430723ddfdd86a6d982795bf9a88f033b3017
Matthew Tsinontas <matthewtsinontas@gmail.com>
2015-11-18 16:57:27 +0000

    Adding in sorting logic to reducer

--- app/src/redux/reducers/learningJourneyReducer.js
+++ app/src/redux/reducers/learningJourneyReducer.js
@@ -1,4 +1,6 @@
 import {LEARNING_JOURNEY, LEARNING_JOURNEY_SUCCESS, LEARNING_JOURNEY_FAIL} from '../actions/learningJourneyActions';
+import _ from 'underscore';
+import moment from 'moment-timezone';
 
 const initialState = {
   waitingForModules: false,
@@ -6,10 +8,11 @@ const initialState = {
 };
 
 export function reducer(state = initialState, action) {
+  var nextState;
   switch (action.type) {
-    case 'LOGOUT': 
+    case 'LOGOUT':
       return {};
-    
+
     case LEARNING_JOURNEY:
       switch (action.status) {
         case "FULFILLED":
@@ -25,6 +28,117 @@ export function reducer(state = initialState, action) {
 
         default: Object.assign({}, state, {waitingForModules: true});
       }
+
+    case "LEARNING_JOURNEY_USER_SELECTED_DATE":
+      nextState = Object.assign({}, state);
+      nextState.currentSelectedDate = action.payload.date;
+      return nextState;
+
+    case "LEARNING_JOURNEY_USER_SELECTED_SLOT":
+      nextState = Object.assign({}, state);
+      nextState.currentSelectedSlot = {
+        slot: action.payload.slot,
+        facilitator: action.payload.facilitator
+      };
+      return nextState;
+
+    case "LEARNING_JOURNEY_BOOKED_SLOT":
+      switch (action.status) {
+        case "RESOLVED":
+          nextState = Object.assign({}, state);
+          if (action.payload !== true) {
+            nextState.error = action.payload;
+          }
+          return nextState;
+
+        default:
+          return state;
+      }
+
+    case "LEARNING_JOURNEY_GET_SLOTS":
+      switch (action.status) {
+        case "RESOLVED":
+          nextState = Object.assign({}, state);
+          nextState.events = action.payload.body.events;
+          return nextState;
+
+        default:
+          return state;
+      }
+
+    case "LEARNING_JOURNEY_REMOVE_ERROR":
+      nextState = Object.assign({}, state);
+      nextState.error = null;
+      return nextState;
+
+    case "LEARNING_JOURNEY_FETCHED":
+      switch (action.status) {
+        case "RESOLVED":
+          return _.mapObject(action.payload, (module, key) => {
+            let activities = _.mapObject(module.activities, (activity, key) => {
+              let status;
+              let slots;
+              var availableSlots = false;
+              //Checking if we have slots, if not, status is TBC
+              for (var i in activity.availableEvents) {
+                if (activity.availableEvents[i].slots.length > 0) {
+                  availableSlots = true;
+                }
+              }
+              if (!availableSlots) {
+                status = "dates-tbc";
+              }
+              //If we have a booked event, proceed to calculate status
+              if (activity.myBookedEventAndSlot) {
+                var bookedEvent = activity.myBookedEventAndSlot;
+                //Checking if we can change the booking or not, by checking todays date vs cancellation date
+                if (moment().isBefore(bookedEvent.cancelBy)) {
+                  status = "booked-can-change";
+                } else {
+                  status = "booked-cannot-change";
+                }
+                //If the event has passed, start to check attendance and rating
+                if (moment().isAfter(bookedEvent.endDate)) {
+                  //If the event has been attended, check logging and rating
+                  if (bookedEvent.attendance === "attended") {
+                    //If the activity is a coaching call then need to check for both rating and log
+                    if (activity.type === "Coaching") {
+                      //If rated, or rated + logged or nothing, then show appropriate statuses
+                      if (bookedEvent.rating) {
+                        status = "rated-coaching";
+                        if (activity.log) {
+                          status = "completed";
+                        }
+                      } else {
+                        status = "attended-coaching";
+                      }
+                    }
+                    //If the activity is a webinar/workshop, only need to check for log
+                    if (activity.type === "Webinar" || activity.type === "Workshop") {
+                      if (activity.log) {
+                        status = "completed";
+                      } else {
+                        status = "attended-activity";
+                      }
+                    }
+                  //IF event is missed or not yet recorded attendance, set accordingly
+                  } else if (bookedEvent.attendance === "missed") {
+                    status = "missed";
+                  } else {
+                    status = "awaiting";
+                  }
+                }
+                //If we dont have a status, then the user can book
+              } else {
+                status = "book";
+              }
+              console.log(status);
+              return {...activity, status};
+            });
+            return {...module, activities};
+          });
+        default: return state;
+      }
     default: return state;
-  } 
+  }
 }
--- app/src/redux/services/learningJourneyService.js
+++ app/src/redux/services/learningJourneyService.js
@@ -1,7 +1,80 @@
 //learningJourneyServices.js
 import config from '../../localConfig';
 import request from 'cirrus/services/request';
+import moment from 'moment-timezone';
+
+let apiRoot = config.api ? config.api.url : '';
 
 export const getlearningModules = () => {
   return Promise.resolve({stub: 'data'});
+};
+
+export const bookSlot = (slotID) => {
+  return request.get(apiRoot + 'api/plj/booking/book/' + slotID);
+};
+
+export const getSlots = (activityID) => {
+  return request.get(`${apiRoot}api/plj/booking/get-slots/${activityID}`);
+};
+
+export const getPLJDataByProgramme = (programmeID) => {
+  //service call goes here
+  return Promise.resolve({
+    //object with top level keys consisting of m+the moduleID (e.g. m1, m2 for module 1 and module 2)
+    m1: {
+      name: "Module 1 - Lorem ipsum dolor sit amet",
+      startDate: "2015-11-05T00:00:00",
+      endDate: "2015-12-05T00:00:00",
+      activities: {
+        //Similar to the module keys, a+activityID (e.g. a1, a2 for activity 1 and 2)
+        a1: {
+          id: 1,
+          name: "Webinar Title Here",
+          description: "Activity description",
+          log: true,
+          properties: null,
+          type: "Coaching",
+          //Custom key, flatten the data for the event and slot if user has booked onto it, otherwise null
+          myBookedEventAndSlot: {
+            name: "Webinar 1",
+            description: "Joining instructions",
+            eventDate: "2015-11-05T00:00:00",
+            facilitator: {
+              id: 1,
+              forname: "Jenny Perkins",
+              surname: ""
+            },
+            startDate: "2015-11-05T00:00:00",
+            endDate: "2015-11-05T00:00:00",
+            location: "Telephone",
+            cancelBy: "2015-11-05T00:00:00",
+            attendance: "attended",
+            rating: true
+          },
+          //Custom key, populate with events + available slots, this will be used by the booking system
+          availableEvents: [{
+            name: "Webinar 1",
+            description: "Joining instructions",
+            eventDate: "2015-11-05T00:00:00",
+            facilitator: {
+              id: 1,
+              forname: "Jenny Perkins",
+              surname: ""
+            },
+            slots: [{
+              startDate: "2015-11-05T00:00:00",
+              endDate: "2015-11-05T00:00:00",
+              location: "Telephone",
+              cancelBy: "2015-11-05T00:00:00"
+            }, {
+              startDate: "2015-11-05T00:00:00",
+              endDate: "2015-11-05T00:00:00",
+              location: "Telephone",
+              cancelBy: "2015-11-05T00:00:00"
+            }]
+          }]
+        }
+      }
+    }
+  });
 };
