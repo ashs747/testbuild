c8249bfbdd4846ce8d7d9c603c9a34db4d95d18d
Matt Cavanagh <matt.cavanagh@darkynt.co.uk>
2015-12-09 17:26:15 +0000

    Interpolate props

--- app/src/react/components/CloudinaryImg.jsx
+++ app/src/react/components/CloudinaryImg.jsx
@@ -12,7 +12,7 @@ class CloudinaryImg extends React.Component {
       metadata = this.props.file.metadata;
     }
     
-    var modStr = '';
+    var modStr = [];
     var file = this.props.file;
     if (file && file.metadata) {
       /*eslint-disable camelcase */
@@ -22,12 +22,37 @@ class CloudinaryImg extends React.Component {
 
       var outStr = (this.props.secure ? file.secure_url : file.url) || '';
 
-      if (file.context === "profile-picture") {
-        modStr = 'w_200,h_200,c_thumb,g_face,';
+      if (file.context) {
+        //defaults
+        switch (file.context) {
+          case "profile-picture":
+            modStr = 'w_200,h_200,c_thumb,g_face,';
+            break;
+        }
       }
 
-      if (file.rotate) {
-        modStr = `a_${file.rotate},${modStr}`;
+      if (file.rotate || this.props.rotate) {
+        if (!this.props.rotate) {
+          modStr = `a_${file.rotate},${modStr}`;
+        } else {
+          let calculateRotate = (file.rotate + this.props.rotate) % 360;
+          modStr = `a_${calculateRotate}`;
+        }
+      }
+
+      for (let key in this.props) {
+        if (this.props.hasOwnProperty(key)) {
+          switch (key) {
+            case 'width':
+              modStr.push(`w_${this.props[key]}`);
+              break;
+            case 'height':
+              modStr.push(`h_${this.props[key]}`);
+              break;
+            case 'crop':
+              modStr.push(`c_${this.props[key]}`);
+          }
+        }
       }
 
       if (modStr) {
@@ -37,18 +62,18 @@ class CloudinaryImg extends React.Component {
       outStr = outStr.split('upload/').join(`upload/${modStr}`);
 
     } else {
+      // format a default
       outStr = this.props.defaultImg || '';
       file = {};
       file.secure_url = outStr;
       file.url = outStr;
-
     }
     /*eslint-enable camelcase */
     var outputImageWithAnchor = (<a href={this.props.secure ? file.secure_url : file.url}>
       <img src={outStr} alt={this.props.alt}/>
     </a>);
 
-    return outputImageWithAnchor;
+    return (this.props.outputTextOnly) ? outStr : outputImageWithAnchor;
   }
 }
 
