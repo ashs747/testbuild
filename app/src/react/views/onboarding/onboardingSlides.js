28f5c846062b53d0e5c58e02f8bd5ecad6798830
Matthew Tsinontas <matthewtsinontas@gmail.com>
2015-12-24 10:18:45 +0000

    Final tweaks to the onboarding styling and slide locations.

--- app/src/react/views/onboarding/onboardingSlides.js
+++ app/src/react/views/onboarding/onboardingSlides.js
@@ -101,9 +101,7 @@ export const onboardingSlides = [{
             <div className="col-sm-5">
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
                 labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.<br /><br />
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                labore et dolore magna aliqua. </p>
+                nisi ut aliquip ex ea commodo consequat.</p>
             </div>
             <div className="clearfix"/>
           </div>
@@ -131,9 +129,7 @@ export const onboardingSlides = [{
             <div className="col-sm-5">
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
                 labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.<br /><br />
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                labore et dolore magna aliqua. </p>
+                nisi ut aliquip ex ea commodo consequat.</p>
             </div>
             <div className="clearfix"/>
           </div>
