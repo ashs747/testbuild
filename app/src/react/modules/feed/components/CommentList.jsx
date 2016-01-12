6f34872f9cb2a779b3b8c56038396098eed7c74c
Matt Cavanagh <matt.cavanagh@darkynt.co.uk>
2016-01-12 11:15:53 +0000

    Properties trauma

--- app/src/react/modules/feed/components/CommentList.jsx
+++ app/src/react/modules/feed/components/CommentList.jsx
@@ -73,10 +73,7 @@ class CommentList extends React.Component {
         return dispatch(updateMessage(this.props.feedID, comment.id, text));
       };
 
-      let properties = {};
-      try {
-        properties = JSON.parse(comment.user.properties);
-      } catch (e) {}
+      var properties = comment.user.properties;
 
       return (<Comment
         key={comment.id}
--- app/src/redux/reducers/cohortReducer.js
+++ app/src/redux/reducers/cohortReducer.js
@@ -7,11 +7,6 @@ export function reducer(state = initialState, action) {
     case 'COHORT_GOT_USER_COHORT':
       var newState = {...state};
       var cohort = action.payload.cohort;
-      cohort.users.forEach(user => {
-        try {
-          user.properties = JSON.parse(user.properties);
-        } catch (e) {}
-      });
       newState = action.payload.cohort;
       return newState;
       break;
