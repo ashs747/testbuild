3893e08cd3f46c9795393de22e4e0e27830cd435
Matt Cavanagh <matt.cavanagh@darkynt.co.uk>
2016-02-05 11:23:15 +0000

    Sorted

--- app/src/react/components/UploadProfile.jsx
+++ app/src/react/components/UploadProfile.jsx
@@ -42,7 +42,8 @@ class UploadProfile extends React.Component {
   }
 
   render() {
-    let loading = this.state.loading ? <img src="assets/img/ajax-loader-red.gif" /> : this.props.buttonText;
+    
+    let loading = (this.state.loading || this.props.profilePicPending) ? <img src="assets/img/ajax-loader-red.gif" /> : this.props.buttonText;
     let content = this.props.width === "sm" ? (
       <div className="row">
         <div className="col-xs-6">
