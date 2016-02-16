03c8e4a7f3ed6d78f6b6d3ff030bb28b9c669d2a
Matt Cavanagh <matt.cavanagh@darkynt.co.uk>
2016-02-16 11:23:19 +0000

    Typo

--- docker/run.sh
+++ docker/run.sh
@@ -16,7 +16,7 @@ then
   ./node_modules/gulp/bin/gulp.js build
   zip -r $versionName.zip ./ -x "*.git*" -x "venv/*" -x "node_modules/*"
   aws s3 cp $versionName.zip s3://cirrus-deployments/soj-portal/$versionName.zip
-  rm $2.zip
+  rm $versionName.zip
   aws elasticbeanstalk create-application-version --application-name "SOJ Portal" --version-label $versionName --source-bundle S3Bucket=cirrus-deployments,S3Key=soj-portal/$versionName.zip
   aws elasticbeanstalk update-environment --environment-name soj-portal-$3 --version-label $versionName
 fi
