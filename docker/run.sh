e020c6137d2b2afd649596832e9fa20fd5df59ff
Matt Cavanagh <matt.cavanagh@darkynt.co.uk>
2016-02-16 11:22:42 +0000

    breakout versionName

--- docker/run.sh
+++ docker/run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+versionName = "${3:0:3}-${2:0:8}"
 if [ "$1" == "deployment" ]
 then
   if [ "$3" == "staging" ]
   then
     cp -f app/src/localConfig.js.staging app/src/localConfig.js
   else
-    if [ "$3" == "production" ]
+    if [ "$3" == "live" ]
     then
       cp -f app/src/localConfig.js.prod app/src/localConfig.js
     else
@@ -13,9 +14,9 @@ then
     fi
   fi
   ./node_modules/gulp/bin/gulp.js build
-  zip -r $2.zip ./ -x "*.git*" -x "venv/*" -x "node_modules/*"
-  aws s3 cp $2.zip s3://cirrus-deployments/soj-portal/$2.zip
+  zip -r $versionName.zip ./ -x "*.git*" -x "venv/*" -x "node_modules/*"
+  aws s3 cp $versionName.zip s3://cirrus-deployments/soj-portal/$versionName.zip
   rm $2.zip
-  aws elasticbeanstalk create-application-version --application-name "SOJ Portal" --version-label $2 --source-bundle S3Bucket=cirrus-deployments,S3Key=soj-portal/$2.zip
-  aws elasticbeanstalk update-environment --environment-name soj-portal-$3 --version-label $2
+  aws elasticbeanstalk create-application-version --application-name "SOJ Portal" --version-label $versionName --source-bundle S3Bucket=cirrus-deployments,S3Key=soj-portal/$versionName.zip
+  aws elasticbeanstalk update-environment --environment-name soj-portal-$3 --version-label $versionName
 fi
