986afd8ef4904ad8c28fd474e1d1f41aae4f4635
divya <divya.renjith@cirrus-connect.com>
2016-05-18 15:38:56 +0100

    furtherchanges

---
+++ app/src/react/components/ToDoList.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Todo from './Todo.jsx';
+
+class ToDoList extends React.Component {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+  	var todoItems = this.props.data.map(function(item) {
+  		return <Todo text={item.text} createdBy={item.createdBy}  date={item.date}/>;
+  	});
+
+    return (
+    	<div>
+    		{todoItems}
+    	</div>
+    )
+  }
+}
+
+export default ToDoList;
