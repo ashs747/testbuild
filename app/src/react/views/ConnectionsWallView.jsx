afd63fd9f774ffe404b3431c7efe521ccbdab3df
chrissnape <chrissnape91@googlemail.com>
2016-07-05 12:12:57 +0100

    Adding the learning journey as props into the conections wall view and using some logic from ActivityView.jsx to parse the activity and module objects

--- app/src/react/views/ConnectionsWallView.jsx
+++ app/src/react/views/ConnectionsWallView.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import ConnectionsWall from '../modules/connectionsWall/ConnectionsWall.jsx';
+import _ from 'underscore';
 
 class ConnectionsWallView extends React.Component {
 
@@ -11,8 +12,17 @@ class ConnectionsWallView extends React.Component {
   render() {
     var walls = this.props.walls;
     var requiredWallId = parseInt(this.props.params.id, 10);
-    var wallObject = null;
+    let moduleWithActivity = this.getModuleWithOnlySingleActivity(this.props.learningJourney, requiredWallId);
+    var activity = _.values(moduleWithActivity.activities);
+    if (activity.length === 0) {
+      return <div />;
+    }
+    activity = activity[0];
 
+    //console.log(moduleWithActivity) THE MODULE WITH activity
+    //console.log(activity) THE ACTIVITY
+
+    var wallObject = null;
     for (let i = 0; i < walls.length; i++) {
       if (walls[i].activityId === requiredWallId) {
         wallObject = walls[i];
@@ -21,13 +31,30 @@ class ConnectionsWallView extends React.Component {
     }
     return <ConnectionsWall wall={wallObject} currentUser={this.props.currentUser} />
   }
+
+  getModuleWithOnlySingleActivity(modules, activityID) {
+    let singleModuleSingleActivity = {};
+    let newObj = _.mapObject(modules, (mod, key) => {
+      var selectedActivity;
+      var activities = _.mapObject(mod.activities, (act, key) => {
+        if (act.id === parseInt(activityID, 10)) {
+          selectedActivity = {[key]: act};
+        }
+      });
+      if (selectedActivity) {
+        singleModuleSingleActivity = {...mod, activities: selectedActivity};
+      }
+    });
+    return singleModuleSingleActivity;
+  }
 }
 
 var mappedConnectionsWallWrapperView = connect(state => {
   return {
     loading: state.wall.loading,
     walls: state.wall.walls,
-    currentUser: state.user.id
+    currentUser: state.user.id,
+    learningJourney: state.learningJourney
   };
 })(ConnectionsWallView);
 
