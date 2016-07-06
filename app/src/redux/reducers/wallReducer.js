b7db38f89f584bba42423bfa11178d9d8e592563
matthewtsinontas <matthewtsinontas@gmail.com>
2016-07-06 16:43:33 +0100

    Reformating the state to display the objects as keys of an object rather than an array as a key of another object.

--- app/src/redux/reducers/wallReducer.js
+++ app/src/redux/reducers/wallReducer.js
@@ -1,25 +1,47 @@
-var defaultState = {
-  loading: false,
-  walls: []
-};
+var defaultState = {};
+
+function updateStateField(state, payload) {
+  var walls = state.walls;
+  var idx;
+  for (var i = 0; i < walls.length; i++) {
+    if (walls[i].id === payload.wallID) {
+      idx = i;
+      break;
+    }
+  }
+  if (walls[idx]) {
+    var newWallPosts = walls[idx].posts.map((post, i) => {
+      if (post.id === payload.postID) {
+        post[payload.field] = payload.value;
+      }
+      return post;
+    })
+  }
+  var newState = state;
+  state.walls[idx].posts = newWallPosts;
+  return {...state, walls};
+}
 
 export const reducer = (state = defaultState, action) => {
-  var newState;
+  var wall;
 
   switch (action.type) {
     case "FETCH_WALLS":
       switch(action.status) {
         case "RESOLVED":
-          return {...state, walls: action.payload, loading: false};
+          var nextState = {...state};
+          action.payload.forEach(wall => {
+            nextState[wall.id] = wall;
+          });
+          return nextState;
         case "PENDING":
-          return {...state, loading: true};
+          return {...state};
         case "REJECTED":
           return state;
       }
       return state;
     case "UPDATE_WALL_POST_FIELD":
-      console.log(action.payload);
-      return state;
+      return newState;
     default:
       return state;
   }
