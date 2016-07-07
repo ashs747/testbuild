45ac0826b86f582df309f5d760ed834c881aabea
chrissnape <chrissnape91@googlemail.com>
2016-07-07 10:58:17 +0100

    styled wall posts

--- app/src/react/modules/connectionsWall/WallPost.jsx
+++ app/src/react/modules/connectionsWall/WallPost.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import ImageView from '../../components/ImageView.jsx';
 import Video from '../../components/Video.jsx';
+import CloudinaryImg from '../../components/CloudinaryImg.jsx';
 import moment from 'moment-timezone';
 import classnames from 'classnames'
 
 class WallPost extends React.Component {
+
   constructor() {
     super();
     this.buildPostWithEvidence = this.buildPostWithEvidence.bind(this);
@@ -20,40 +23,73 @@ class WallPost extends React.Component {
   buildEvidenceComponent(evidence) {
     switch (evidence.type) {
       case "image":
-        return <ImageView src = {evidence.url}/>;
+        return <div className="post-evidence"><ImageView layout="box-to-image" style={{ height:"100%", width:"100%"}} src={evidence.url}/></div>
       case "video":
-        return <Video url = {evidence.url}/>;
+        return <div className="post-evidence"><Video url={evidence.url}/></div>
       default:
         return <div>The evidence is not a suitable file format to be displayed</div>
     }
   }
 
   buildPostWithEvidence() {
-    const postClass = classnames('wall-post', 'with-evidence', {'users-post': this.props.postBelongsToUser});
-    const date = moment(this.props.date).format('Do MMMM YYYY');
+    let userLiked = false;
+    for (let i = 0; i < this.props.likes.length; i++) {
+      console.log(this.props.likes[i] + " " + this.props.currentUser);
+      if (this.props.likes[i] === this.props.currentUser){
+        userLiked = true;
+      }
+    }
+    const postClass = classnames('wall-post', 'with-evidence', {'users-post': this.props.postBelongsToUser}, {'post-liked': userLiked});
+    const date = moment(this.props.date).format('DD.MM.YYYY');
     const evidenceComponent = this.buildEvidenceComponent(this.props.evidence);
+    const label = this.getUserLabel(this.props.postBelongsToUser);
     return (
       <div className={postClass}>
-        <h5>{this.props.title}</h5>
-        <div className="post-user">{this.props.owner.forename} {this.props.owner.surname}</div>
-        <div className="post-date">{date}</div>
-        <p>{this.props.description}</p>
+        {label}
         {evidenceComponent}
-        <div>{this.props.likes.length} <button>Like</button></div>
+        <h5>{this.props.title}</h5>
+        <div className="post-details">
+          <div className="post-name"><CloudinaryImg file="" defaultImg="assets/img/profile-placeholder.jpg" /> {this.props.owner.forename} {this.props.owner.surname}</div>
+          <div className="post-date">Uploaded: {date}</div>
+        </div>
+        <div className="post-like"><button><i className="fa fa-thumbs-o-up"></i></button>{this.props.likes.length}</div>
       </div>
     )
   }
 
   buildPostWithoutEvidence() {
     const postClass = classnames('wall-post', 'without-evidence', {'users-post': this.props.postBelongsToUser});
+    let uploadButton = (this.props.postBelongsToUser) ? <button className="upload-evidence">+ Upload</button> : null
+    const label = this.getUserLabel(this.props.postBelongsToUser);
     return (
       <div className={postClass}>
-        <h5>No Evidence Yet</h5>
-        <div className="post-user">{this.props.owner.forename} {this.props.owner.surname}</div>
-        <img className="placeholder-image" src="http://res.cloudinary.com/strata/image/upload/v1467020297/placeholder_c6u3x0.png"/>
-        <button>Upload</button>
+        {label}
+        <div className="post-evidence">
+          <img className="placeholder-image" src="http://res.cloudinary.com/strata/image/upload/v1467878082/connections-wall-awaiting-upload_ecaku1.png"/>
+        </div>
+        <h5>Awaiting Title</h5>
+        <div className="post-details">
+          <div className="post-name"><CloudinaryImg file="" defaultImg="assets/img/profile-placeholder.jpg" /> {this.props.owner.forename} {this.props.owner.surname}</div>
+        </div>
+        {uploadButton}
       </div>
     )
   }
+
+  getUserLabel(user) {
+    if (user) {
+      return <img className="users-post-label"src="http://res.cloudinary.com/strata/image/upload/v1467819173/connections-wall-you_uxihlm.png" />;
+    }
+    else{
+      return null
+    }
+  }
 }
-export default WallPost;
+
+var mappedWallPost = connect(state => {
+  return {
+    currentUser: state.user.id,
+  };
+})(WallPost);
+
+export default mappedWallPost;
